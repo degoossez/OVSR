095aceb6754149e61d92c3015b47ba480209abc8
degoossez <driesgoossens93@gmail.com>
2014-03-24 17:30:14 +0100

    Moved the submit button to the correct layout. No need for a submit button in the console.

--- gen/com/denayer/ovsr/R.java
+++ gen/com/denayer/ovsr/R.java
@@ -22,30 +22,35 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int Delete=0x7f09000b;
-        public static final int File=0x7f09000d;
+        public static final int CodeTab=0x7f090007;
+        public static final int ConsoleTab=0x7f09000a;
+        public static final int Delete=0x7f090010;
+        public static final int EditText01=0x7f09000b;
+        public static final int File=0x7f090012;
         public static final int FilterButton=0x7f090005;
-        public static final int History=0x7f09000c;
-        public static final int Home=0x7f09000a;
-        public static final int LoadF=0x7f09000f;
+        public static final int History=0x7f090011;
+        public static final int Home=0x7f09000f;
+        public static final int LoadF=0x7f090014;
         public static final int LogField=0x7f090000;
-        public static final int SaveF=0x7f09000e;
-        public static final int Template=0x7f090010;
-        public static final int action_settings=0x7f090009;
-        public static final int editText1=0x7f090007;
+        public static final int LogTab=0x7f09000c;
+        public static final int SaveF=0x7f090013;
+        public static final int Template=0x7f090015;
+        public static final int action_settings=0x7f09000e;
+        public static final int editText1=0x7f090008;
         public static final int imageButton1=0x7f090001;
         public static final int imageButton2=0x7f090002;
         public static final int radioButton1=0x7f090004;
         public static final int radioButton2=0x7f090003;
-        public static final int submit_button=0x7f090008;
-        public static final int timeview=0x7f090006;
+        public static final int submit_button=0x7f090009;
+        public static final int tabhost=0x7f090006;
+        public static final int timeview=0x7f09000d;
     }
     public static final class layout {
-        public static final int activity_activity_test=0x7f030000;
-        public static final int activity_display_message=0x7f030001;
-        public static final int activity_main=0x7f030002;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_test=0x7f080000;
