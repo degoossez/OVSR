61f86f387359ac979416f7dd62957ae074a467ac
KoenDaelman <koendaelman@gmail.com>
2014-04-02 10:57:38 +0200

    "tcpClient edited"

--- src/com/denayer/ovsr/RsScript.java
+++ src/com/denayer/ovsr/RsScript.java
@@ -383,36 +383,10 @@ public class RsScript extends Object {
 	}
 	public void codeFromFile(String code)
 	{
-		//implement function
-		runTcpClient("Hi");
-		
+			
 	}	
 	
-	private String runTcpClient(String sendMessage) {
-		String inMsg = "";	//message from server
-		
-	    try {
-	        Socket s = new Socket(TCP_SERVER_IP, TCP_SERVER_PORT);
-	        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-	        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-	        //send output msg
-	        String outMsg = "TCP connecting to " + TCP_SERVER_PORT + System.getProperty("line.separator"); 
-	        out.write(outMsg);
-	        out.flush();
-	        Log.i("TcpClient", "sent: " + outMsg);
-	        //accept server response
-	        inMsg = in.readLine() + System.getProperty("line.separator");
-	        Log.i("TcpClient", "received: " + inMsg);
-	        //close connection
-	        s.close();
-	    } catch (UnknownHostException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } 
-	    
-	    return inMsg;
-	}
+	
 	
 	
 }
