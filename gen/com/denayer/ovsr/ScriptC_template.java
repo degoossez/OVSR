3e7511cc8c32d684c6b96a14dabe555ead488d72
KoenDaelman <koendaelman@gmail.com>
2014-05-19 14:08:19 +0200

    "changes "

--- gen/com/denayer/ovsr/ScriptC_template.java
+++ gen/com/denayer/ovsr/ScriptC_template.java
@@ -16,7 +16,7 @@
 
 /*
  * This file is auto-generated. DO NOT MODIFY!
- * The source Renderscript file: /home/dries/git/OVSR/src/com/denayer/ovsr/template.rs
+ * The source Renderscript file: /home/koen/git/OVSR/src/com/denayer/ovsr/template.rs
  */
 package com.denayer.ovsr;
 
